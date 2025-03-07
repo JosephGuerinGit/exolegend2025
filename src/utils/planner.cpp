e150c9e776d2e5f6bd76d32e4961d66bf859dd7c
Andeol <andeol.moulin@ensta-paris.fr>
2025-03-07 21:20:34 +0100

    new commit

--- src/utils/planner.cpp
+++ src/utils/planner.cpp
@@ -10,42 +10,22 @@
 
 using namespace std;
 
-
-struct Node {   //utiliser ici le struct MazeSquare (et rajouter dans mazesquare)
-    MazeSquare* node;
-    int g, h, f;
-    bool in_closed_list;
-    Node* parent;
-    Node(MazeSquare* node, int g, int h, Node* parent = nullptr) 
-         : node(node), g(g), h(h), f(g + h), parent(parent) {}
-};
-
-
-struct CompareNodes {     //compare le f_score de a et b (et retourne true si a>b)
-    bool operator()(const Node* a, const Node* b) {
-        return a->f > b->f;
-    }
- };
-
 int heuristic(int x1, int y1, int x2, int y2) {   //distance de Manhattan
     return abs(x1 - x2) + abs(y1 - y2);
 }
 
-
-
-
 LinkedList defineNewPath(MazeSquare* start ,MazeSquare*goal) {
     int rows = 12, cols = 12;
-    priority_queue<Node*, vector<Node*>, CompareNodes> openList;    //openlist
-    Node* allNodes[rows][cols] = {nullptr};  // All elements initially are not created
+    priority_queue<Nodes*, vector<Nodes*>, CompareNodes> openList;    //openlist
+    Nodes* allNodes[rows][cols] = {nullptr};  // All elements initially are not created
     
-    Node* startNode = new Node(start, 0, heuristic(start->j, start->i, goal->j, goal->i));
+    Nodes* startNode = new Nodes(start, 0, heuristic(start->j, start->i, goal->j, goal->i));
     openList.push(startNode);
     allNodes[start->j-'0'][start->i-'0'] = startNode;
     
     
     while (!openList.empty()) {
-        Node* current = openList.top();
+        Nodes* current = openList.top();
         openList.pop();
         MazeSquare *Current = current->node;
         
@@ -53,7 +33,6 @@ LinkedList defineNewPath(MazeSquare* start ,MazeSquare*goal) {
 
         if (Current->j == goal->j && Current->i == goal->i) {  //if we reached the goal
             LinkedList path;            
-            vector<pair<int, int>> path;     //initialization of the path vector
             while (Current) {
                 path.push_back(Current);
                 Current = current->parent->node;
@@ -80,7 +59,7 @@ LinkedList defineNewPath(MazeSquare* start ,MazeSquare*goal) {
             if (x >= 0 && x < rows && y >= 0 && y < cols && neighbor!=nullptr && !current->in_closed_list) {
                 int newG = current->g + 1;
                 if (!allNodes[x][y] || newG < allNodes[x][y]->g) {
-                    Node* Neighbor = new Node(neighbor, newG, heuristic(x, y, goal->j, goal->i), current);
+                    Nodes* Neighbor = new Nodes(neighbor, newG, heuristic(x, y, goal->j, goal->i), current);
                     openList.push(Neighbor);       //on donne un MazeSquare alors qu'on veut 
                     allNodes[x][y] = Neighbor;  
                 }
@@ -91,14 +70,17 @@ LinkedList defineNewPath(MazeSquare* start ,MazeSquare*goal) {
 }
 
 
-void PrintPath(vector<pair<int, int>> path) {
-    for (const auto& p : path) {
-        cout << "(" << (int)p.first << ", " << (int)p.second << ")\n";
-    }
+MazeSquare* getNewSquare(){
+    MazeSquare* Node_try;
+    
+
+    Node_try->i=0+'0';Node_try->j=0+'0';
+    return(Node_try);
 }
 
 
-Position* getNewPosition(MazeSquare* mazesquare, float squareSize){
+Position* getNewPosition(float squareSize){
+    MazeSquare* mazesquare = getNewSquare();
     // calculons les coordonnées du centre de cette case
     Position centerCoor;
     // pour calculer les coordonnées x et y il faut récupérer les index i et j de la case
@@ -106,7 +88,7 @@ Position* getNewPosition(MazeSquare* mazesquare, float squareSize){
     centerCoor.y = (mazesquare->j + 0.5) * squareSize;
 }
 
-"""
+/*
 int main() {
     
     MazeSquare* Start_Node;
@@ -120,14 +102,12 @@ int main() {
     vector<pair<int, int>> path = findPath(Start_Node, Goal_Node);
     
     if (!path.empty()) {
-         cout << 'Path found:\n';
+         cout << "Path found:\n";
     }
     else {
-        cout << 'No path found.' << endl;
+        cout << "No path found."" << endl;
     }
     delete Start_Node;
     delete Goal_Node;
     return 0;
-}
-"""
-
+}*/
